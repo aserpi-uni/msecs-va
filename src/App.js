34dbc598cfd9b37895dfbddd2f59e3b4eec15441
Alessandro Serpi <serpi.1647244@studenti.uniroma1.it>
2020-02-09 16:19:00 +0100

    Fix wrong padding

--- src/App.js
+++ src/App.js
@@ -8,15 +8,15 @@ import Elbow from "./Elbow";
 function App() {
     const margin = {x: 50, y: 50, intra: 50},
       fakePadding = 4,
-      firstRowWidth = (document.documentElement.clientWidth - fakePadding - 2*margin.x - 2*margin.intra) / 3,
-      baseHeight = (document.documentElement.clientHeight - 2*margin.y - margin.intra) / 2;
+      commonHeight = (document.documentElement.clientHeight - fakePadding - 2*margin.y - margin.intra) / 2,
+      firstRowWidth = (document.documentElement.clientWidth - 2*margin.x - 2*margin.intra) / 3;
 
     return (
         <div className="App">
             <svg id="baseSvg"
                  height={document.documentElement.clientHeight - fakePadding}
                  width={document.documentElement.clientWidth}>
-                <Elbow height={baseHeight} width={firstRowWidth} margin={margin}/>
+                <Elbow height={commonHeight} width={firstRowWidth} margin={margin}/>
             </svg>
         </div>
     );
