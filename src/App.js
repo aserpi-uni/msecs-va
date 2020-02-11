b672090cb79b6f546dc28973c7af1a3d15f6c249
Alessandro Serpi <serpi.1647244@studenti.uniroma1.it>
2020-02-11 22:43:28 +0100

    Migrate App component to class

--- src/App.js
+++ src/App.js
@@ -1,30 +1,32 @@
 import React from 'react';
+
 import './App.scss';
-
-
 import Elbow from "./Elbow";
 
-// Need 4px of fake padding, otherwise Firefox displays scrollbars
-function App() {
-    function updateFromElbow(new_k) {
-        // TODO: update other visualisations
+
+// TODO: use an alternative id method than HTML id
+class App extends React.Component {
+    render() {
+        const margin = {x: 50, y: 50, intra: 50},
+          fakePadding = 4,  // Need 4px of fake padding, otherwise Firefox displays scrollbars
+          commonHeight = (document.documentElement.clientHeight - fakePadding - 2*margin.y - margin.intra) / 2,
+          firstRowWidth = (document.documentElement.clientWidth - 2*margin.x - 2*margin.intra) / 3;
+
+        return (
+          <div className="App">
+              <svg id="baseSvg"
+                   height={document.documentElement.clientHeight - fakePadding}
+                   width={document.documentElement.clientWidth}>
+                  <Elbow height={commonHeight} width={firstRowWidth} margin={margin}
+                         onRunChange={this.updateFromElbow}/>
+              </svg>
+          </div>
+        )
     }
 
-    const margin = {x: 50, y: 50, intra: 50},
-      fakePadding = 4,
-      commonHeight = (document.documentElement.clientHeight - fakePadding - 2*margin.y - margin.intra) / 2,
-      firstRowWidth = (document.documentElement.clientWidth - 2*margin.x - 2*margin.intra) / 3;
-
-    return (
-        <div className="App">
-            <svg id="baseSvg"
-                 height={document.documentElement.clientHeight - fakePadding}
-                 width={document.documentElement.clientWidth}>
-                <Elbow height={commonHeight} width={firstRowWidth} margin={margin}
-                         onRunChange={this.updateFromElbow}/>
-            </svg>
-        </div>
-    );
+    updateFromElbow(new_k) {
+        // TODO: update other visualisations
+    }
 }
 
 export default App;
